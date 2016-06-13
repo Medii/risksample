48f463a51aa44c5485bb9a33b3ceccff5d677e1b
Mehdi Khezrian <mkhezrian@yahoo.com>
2016-06-13 23:01:02 +1000

    Added API test cases

---
+++ Sample.Risk.Api.Tests/IntegrationTest.cs
@@ -0,0 +1,44 @@
+﻿namespace Sample.Risk.Api.Tests
+{
+    using System.Collections.Generic;
+    using System.Net.Http;
+
+    using Microsoft.Owin.Hosting;
+
+    using Newtonsoft.Json;
+
+    using NUnit.Framework;
+
+    using Sample.Risk.Common.Dto;
+
+    [TestFixture]
+    public class IntegrationTest
+    {
+        #region Public Methods
+
+        [Explicit]
+        [Test]
+        public void ApiTest()
+        {
+            string baseAddress = "http://localhost:9000/";
+            StartOptions options = new StartOptions(baseAddress) { ServerFactory = "Microsoft.Owin.Host.HttpListener" };
+            options.Urls.Add("http://localhost:9000");
+            options.Urls.Add("http://127.0.0.1:9000");
+            using (WebApp.Start<Startup>(baseAddress))
+            {
+                HttpClient client = new HttpClient();
+                HttpResponseMessage response = client.GetAsync(baseAddress + "Api/BettingRisk/Customer").Result;
+                List<CustomerDto> customers = JsonConvert.DeserializeObject<List<CustomerDto>>(response.Content.ReadAsStringAsync().Result);
+                Assert.IsNotNull(customers);
+                Assert.AreEqual(6, customers.Count);
+
+                response = new HttpClient().GetAsync(baseAddress + "Api/BettingRisk/UnsettledBet").Result;
+                List<UnsettledDto> unsettled = JsonConvert.DeserializeObject<List<UnsettledDto>>(response.Content.ReadAsStringAsync().Result);
+
+                Assert.AreEqual(22, unsettled.Count);
+            }
+        }
+
+        #endregion
+    }
+}
--- Sample.Risk.Api/Program.cs
+++ Sample.Risk.Api/Program.cs
@@ -1,8 +1,14 @@
 ﻿namespace Sample.Risk.Api
 {
     using System;
+    using System.Collections.Generic;
+    using System.Net.Http;
 
     using Microsoft.Owin.Hosting;
+
+    using Newtonsoft.Json;
+
+    using Sample.Risk.Common.Dto;
 
     public class Program
     {
@@ -11,8 +17,11 @@
         private static void Main()
         {
             string baseAddress = "http://localhost:9000/";
-
-            using (WebApp.Start<Startup>(baseAddress))
+            StartOptions options = new StartOptions(baseAddress) { ServerFactory = "Microsoft.Owin.Host.HttpListener" };
+            options.Urls.Add("http://localhost:9000");
+            options.Urls.Add("http://127.0.0.1:9000");
+            //options.Urls.Add(string.Format("http://{0}:9000", Environment.MachineName));
+            using (WebApp.Start<Startup>(options))
             {
                 Console.WriteLine("Betting API service started...");
                 Console.WriteLine("Available services: ");
@@ -20,9 +29,6 @@
                 Console.WriteLine("http://localhost:9000/Api/BettingRisk/Customer/Api/BettingRisk/UnsettledBet");
                 Console.WriteLine("Press enter to shutdown the service");
                 Console.ReadLine();
-                //HttpResponseMessage response = client.GetAsync(baseAddress + "Api/BettingRisk/Customer").Result;
-                //Console.WriteLine(response);
-                //Console.WriteLine(response.Content.ReadAsStringAsync().Result);
             }
         }
 
