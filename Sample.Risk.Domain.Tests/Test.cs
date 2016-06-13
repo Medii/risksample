fecf5100e1b87039c6d1c85f009f429b3be418af
Mehdi Khezrian <mkhezrian@yahoo.com>
2016-06-13 17:14:06 +1000

    Updated unsettled get test case

--- Sample.Risk.Domain.Tests/Test.cs
+++ Sample.Risk.Domain.Tests/Test.cs
@@ -16,6 +16,8 @@
 
         private List<SettledDto> settledDtos;
 
+        private List<UnsettledDto> unsettledDtos;
+
         #endregion
 
         #region Public Methods
@@ -31,6 +33,14 @@
             this.settledDtos.Add(new SettledDto { Punter = new CustomerDto { Id = 2 }, Stake = 200, Win = 0, EventId = 2, ParticipantId = 1 });
             this.settledDtos.Add(new SettledDto { Punter = new CustomerDto { Id = 3 }, Stake = 20, Win = 1100, EventId = 1, ParticipantId = 1 });
             this.settledDtos.Add(new SettledDto { Punter = new CustomerDto { Id = 3 }, Stake = 20, Win = 0, EventId = 2, ParticipantId = 1 });
+
+            this.unsettledDtos = new List<UnsettledDto>();
+            this.unsettledDtos.Add(new UnsettledDto { Punter = new CustomerDto { Id = 1 }, Stake = 20, ToWin = 100, EventId = 3, ParticipantId = 1 });
+            this.unsettledDtos.Add(new UnsettledDto { Punter = new CustomerDto { Id = 1 }, Stake = 200, ToWin = 1100, EventId = 3, ParticipantId = 2 });
+            this.unsettledDtos.Add(new UnsettledDto { Punter = new CustomerDto { Id = 2 }, Stake = 100, ToWin = 200, EventId = 3, ParticipantId = 1 });
+            this.unsettledDtos.Add(new UnsettledDto { Punter = new CustomerDto { Id = 2 }, Stake = 2000, ToWin = 3000, EventId = 3, ParticipantId = 2 });
+            this.unsettledDtos.Add(new UnsettledDto { Punter = new CustomerDto { Id = 3 }, Stake = 220, ToWin = 300, EventId = 3, ParticipantId = 1 });
+            this.unsettledDtos.Add(new UnsettledDto { Punter = new CustomerDto { Id = 3 }, Stake = 610, ToWin = 700, EventId = 4, ParticipantId = 1 });
         }
 
         [Test]
@@ -39,13 +49,34 @@
             Mock<IBetProvider> mockProvider = new Mock<IBetProvider>();
             mockProvider.Setup(p => p.GetSettledDto()).Returns(this.settledDtos);
             RiskBal riskBal = new RiskBal(mockProvider.Object);
-            Dictionary<int, CustomerDto> result = riskBal.GetCustomers();
-            Assert.AreEqual(3, result.Count);
-            Assert.AreEqual(70, result[1].AverageStake);
-            Assert.AreEqual(true, result[1].UnusuallyWins);
-            Assert.AreEqual(true, result[1].UnusuallyWins);
+            Dictionary<int, CustomerDto> customers = riskBal.GetCustomers();
 
+            Assert.AreEqual(3, customers.Count);
+            Assert.AreEqual(70, customers[1].AverageStake);
+            Assert.AreEqual(true, customers[1].UnusuallyWins);
+            Assert.AreEqual(150, customers[2].AverageStake);
+            Assert.AreEqual(false, customers[2].UnusuallyWins);
+            Assert.AreEqual(20, customers[3].AverageStake);
+            Assert.AreEqual(false, customers[3].UnusuallyWins);
+        }
 
+        [Test]
+        public void GetUpcomingBets_Should_Return_List_Of_Upcoming_Bets_By_Processing_Recieved_Customers_And_Fetched_UnsettledDtos()
+        {
+            Mock<IBetProvider> mockProvider = new Mock<IBetProvider>();
+            mockProvider.Setup(p => p.GetSettledDto()).Returns(this.settledDtos);
+            mockProvider.Setup(p => p.GetUnsettledDtos()).Returns(this.unsettledDtos);
+            RiskBal riskBal = new RiskBal(mockProvider.Object);
+            Dictionary<int, CustomerDto> customers = riskBal.GetCustomers();
+
+            List<UnsettledDto> unsettled = riskBal.GetUpcomingBets(customers);
+            Assert.AreEqual(6, unsettled.Count);
+            Assert.AreEqual(RiskTypes.Risky, unsettled[0].RiskType);
+            Assert.AreEqual(RiskTypes.Unusual | RiskTypes.Risky | RiskTypes.StakesGreatherThanThousand, unsettled[1].RiskType);
+            Assert.AreEqual(RiskTypes.Normal, unsettled[2].RiskType);
+            Assert.AreEqual(RiskTypes.Unusual | RiskTypes.StakesGreatherThanThousand, unsettled[3].RiskType);
+            Assert.AreEqual(RiskTypes.Unusual, unsettled[4].RiskType);
+            Assert.AreEqual(RiskTypes.Unusual | RiskTypes.HighlyUnusual, unsettled[5].RiskType);
         }
 
         #endregion
